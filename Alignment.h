46ab8d8a0135d0951202e9b65f6c7769d388f233
hashemifar <hashemifar@ttic.edu>
2015-08-25 21:45:58 -0500

    Update Alignment.h

--- Alignment.h
+++ Alignment.h
@@ -11,6 +11,9 @@ using namespace std;
 class Alignment
 {
 public:
+	//measures for evaluating the alignment
+    float EC, S3;
+	int CCCV,CCCE;
 
 	int *alignment; //array of alignment of two networks
 	int *comp; //array of connected components in the resulted alignment
@@ -24,7 +27,7 @@ public:
     //constructor
     //finds the smaller network and the maximum degree of the input networks
     //Inputs are two files of networks net1 and net2
-	Alignment( Network& net1, Network& net2);
+	Alignment( Network net1, Network net2);
 	
     //constructor.does nothing !
     Alignment(void);
@@ -34,8 +37,30 @@ public:
     
     //produce a mapping between nodes of two network with respect to input parameter a. 
     //Input parameter a acontrols the factor edgeWeight in assigning the scores to the nodes. a should be between 0 and 1.
-    void align(double a);
+    void align(double lambda, double alpha);
+    
+    //calculate the evaluation measurments EC (Edge Correctness), IC (Interaction Correctness), NC (Node Correctness), CCCV and CCCE (largest Common Connected subraph with recpect to Vertices and Edges)
+	void evaluate(void);
+        
+	//calculate CCCV
+    //return the number of vertices of largest common connected subgraph of the alignment
+    int getCCCV(void);
+    
+    //calculate the evaluation measurment CCCE
+    //return the number of edges of largest common connected subgraph of the alignment
+    int getCCCE(void);
+
+    //calculate the evaluation measurment EC
+    //returns the percent of edges that are mapped correctly in alignment 
+	float getEC(void);
+        
+    float getS3(void);
+
+	//Input parameter name determines the file that result are to be written in.
+    void outputEvaluation(string name);
+    
     //print the alignment(mapping) in a file with input parameter name
 	//Input parameter name determines the file that mapping is to be written in.    
 	void outputAlignment(string name);
+    void readblast(string blastFile);
 };
